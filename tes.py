18a6b0364086ff9aa4c6108d74a1324d7864b5b4
quentin <quentin.sornin@edu.ece.fr>
2024-10-09 17:41:57 +0200

    test

--- tes.py
+++ tes.py
@@ -1,2 +1,16 @@
-test = 0
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+data_file = './data_lab1 - copie.txt'
+
+columns = ['x','y']
+
+dataset = pd.read_csv(data_file,names = columns, sep=' ')
+
+
+dataset.plot(kind='scatter',x='x',y='y',color='blue')
+
+plt.show()
+
+print(dataset)
